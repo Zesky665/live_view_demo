41fdb2d0181eb12f39eedddc66cfa427eff2b234
Zesky665 <zharkoc@gmail.com>
2019-09-28 22:33:10 +0200

    setup for render

--- lib/live_view_demo/repo.ex
+++ lib/live_view_demo/repo.ex
@@ -1,5 +1,10 @@
 defmodule LiveViewDemo.Repo do
   use Ecto.Repo,
     otp_app: :live_view_demo,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    pool_size: 10
+
+    def init(_type, config) do
+      {:ok, Keyword.put(config, :url, System.get_env("DATABSE_URL"))}
+    end  
 end
