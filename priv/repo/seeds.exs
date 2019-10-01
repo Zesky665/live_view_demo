f4d522896cb4da68529ff66f4b75680d56203c13
Zesky665 <zharkoc@gmail.com>
2019-10-01 21:40:43 +0200

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,22 +7,11 @@
 #
     LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_1"})
 
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_2"})
+    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "This is a demo", line_two: "Of my Haiku Writing App", line_three: "For Phoenix Phenzy", author: "Zesky665"})
 
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_3"})
+    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "Visiting a page", line_two: "Updates without refresh", line_three: "It must be LiveView", author: "Mike Buhot"})
 
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_4"})
-
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_5"})
-
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_6"})
-
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_7"})
-
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_8"})
-
-    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "With Phoenix Live View", line_two: "Writing haiku in Real Time", line_three: "Is a piece of cake", author: "Tester_9"})
-
+    LiveViewDemo.Repo.insert!(%LiveViewDemo.Poems.Haiku{line_one: "Phoenix is real cool", line_two: "Speedy, performant, easy", line_three: "My new go to tool", author: "Steve Albrechtsen"})
 
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
